15b704d2c0c83ab04d9f8bf8f1da2c6c37894c65
Kseniia30 <kellyshikova3004@gmail.com>
2022-08-14 13:34:39 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+
+
+const galleryListTwo = document.querySelector(".gallery");
+const createMarkupTwo = createImagesMarkup(galleryItems);
+galleryListTwo.insertAdjacentHTML("beforeend", createMarkupTwo);
+
+function createImagesMarkup(galleryItems) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `<a class="gallery__link" href="${original}" data-lightbox="lbox" onclick="event.preventDefault()">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+    </a>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox('.gallery__link', { 
+    navText: "<>",
+    captionsData: "alt",
+    captionDelay: 250,
+
+});
