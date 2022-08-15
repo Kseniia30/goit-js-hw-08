c61dc396b8aa5524599aa61667ca89b982295b1a
Kseniia30 <kellyshikova3004@gmail.com>
2022-08-15 11:29:07 +0200

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,34 @@
-    const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+import Player from '@vimeo/player';
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const currentTime = 'videoplayer-current-time';
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+function onPlay(data) {
+    console.log(data);
+    localStorage.setItem(currentTime, JSON.stringify(data))
+}
+    player.on('timeupdate', onPlay);
+
+function returnPlayback() {
+    if (JSON.parse(localStorage.getItem(currentTime)) === null) {
+        return;
+    }
+    const paused = JSON.parse(localStorage.getItem(currentTime))["seconds"]
+
+    if (paused) {
+        player
+            .setCurrentTime(paused)
+            .then(function (seconds) { })
+            .catch(function (error) {
+                switch (error.name) {
+                    case "Error":
+                        break;
+                    default: break;
+                }
+            })
+    }
+    }
+returnPlayback()
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from "lodash.throttle";
+import Lodash from "lodash.throttle";
 
+const key = "feedback-form-state";
+const feedBack = document.querySelector(".feedback-form");
+const mail = document.querySelector("input[name=email]");
+const message = document.querySelector("textarea[name=message]");
+const btn = document.querySelector("button[type=submit]");
+
+feedBack.addEventListener('input', throttle(onInput, 500));
+
+function onInput() {
+    const form = new FormData(feedBack);
+    let newForm = {};
+    form.forEach((value, name) => (newForm[name] = value));
+    localStorage.setItem(key, JSON.stringify(newForm));
+}
+
+function saveInf() {
+    let newFormInf = localStorage.getItem('key');
+    if (newFormInf) {
+        newFormInf = JSON.parse(newFornInf);
+        console.log(newFormInf);
+        Object.entries(newFormInf).forEach(([name, value]) => {
+            form.elements[name].value = value;
+        })
+    }
+}
+saveInf()
+
+feedBack.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+    evt.preventDefault();
+    localStorage.removeItem(key);
+    let infData = {};
+    const formData = new FormData(feedBack);
+    formData.forEach((value, name) => (infData[name] = value));
+    console.log(infData);
+    feedBack.reset();
+}
